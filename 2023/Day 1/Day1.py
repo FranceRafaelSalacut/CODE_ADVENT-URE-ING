4592fd1b5f9660fd8ce71a5656ab244cf45ea58c
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-03-08 11:04:15 +0800

    Added a working solution for Day 1

--- 2023/Day 1/Day1.py
+++ 2023/Day 1/Day1.py
@@ -1,1 +1,32 @@
-print("Lets go adventing stufff")
+'''
+Resources
+https://www.geeksforgeeks.org/python-extract-numbers-from-string/
+https://stackoverflow.com/questions/7961499/best-way-to-loop-over-a-python-string-backwards
+'''
+
+# Constants
+PATH = "2023/Day 1/"
+text_file = "input.txt"
+
+
+temp = []
+
+with open(PATH + text_file, 'r') as file:
+    for line in file:
+        temp.append(line)
+
+sum = 0
+for x in range(0,5):
+    print(temp[x])
+    num1 = num2 = 0
+    for char in temp[x]:
+        if char.isnumeric():
+            num1 = int(char)
+            break
+
+    for char in reversed(temp[x]):
+        if char.isnumeric():
+            num2 = int(char)
+            break
+    
+    print(f"{num1} + {num2} = {num1 + num2}")
