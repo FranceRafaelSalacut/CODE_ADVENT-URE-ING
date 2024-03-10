b9bc11efdb4e77f8e03d82c885596b90147faa77
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-03-10 11:46:57 +0800

    Added an input parser for Day 3

--- 2023/Day 3/Day3.py
+++ 2023/Day 3/Day3.py
@@ -3,11 +3,21 @@ PATH = "2023/Day 3/"
 text_file = "input.txt"
 
 temp = []
+index = 0
 with open(PATH + text_file, 'r') as file:
     for line in file:
-        if len(temp) < 5:
-            temp.append(line)
+        if len(temp) < 2:
+            line = line.split("\n")[0]
+            tempo = []
+            for char in line:
+                tempo.append(char)
+            temp.append(tempo)
         else:
             break
 
-print(temp)
+
+
+for x in range(0, len(temp)):
+    for y in range(0, len(temp[x])):
+        print(temp[x][y], end=" ")
+    print("\n---------------------------------------------------------------------------")
