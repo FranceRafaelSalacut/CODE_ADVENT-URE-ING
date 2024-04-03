dfe411c0a037a94842dd509a747cb5b526223b2e
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-04-03 18:28:14 +0800

    solved Day 3 part 1

--- 2023/Day 3/Day3P1.py
+++ 2023/Day 3/Day3P1.py
@@ -1,5 +1,6 @@
 from icecream import ic #for debugging
 import os
+import time
 #ic.disable()
 
 # Constants
@@ -10,11 +11,11 @@ temp = []
 index = 0
 with open(PATH + text_file, 'r') as file:
     for line in file:
-        if len(temp) < 5:
+        #if len(temp) < 5:
             line = line.split("\n")[0]
             temp.append(line)
-        else:
-            break
+        #else:
+        #    break
 
 def print_given():
     for x in temp:
@@ -31,33 +32,48 @@ def do_box(U_input, index1, indexes):
     # Checking the top 
     if index1 != 0:
         #top-left
-        print(U_input[top_line][first_index-1], end="")
+        if first_index != 0:
+            print(U_input[top_line][first_index-1], end="")
+        else:
+            print(end="")
         #direct-top
         for x in indexes:
             print(U_input[top_line][x], end="")
         #top-right
-        print(U_input[top_line][last_index + 1])
+        if last_index != len(U_input[index1])-1:
+            print(U_input[top_line][last_index + 1])
+        else:
+            print()
 
     # Checking sides
     #left
     if first_index != 0:
         print(U_input[index1][first_index-1], end="")
+    else:
+            print(end="")
 
     for x in indexes:
             print(U_input[index1][x], end="")
     #right
     if last_index != len(U_input[index1])-1:
         print(U_input[index1][last_index+1])
+    else:
+        print()
 
     # Checking the bottom
     if index1 != len(U_input)-1:
         #bottom-left
-        print(U_input[bottom_line][first_index-1], end="")
+        if first_index != 0:
+            print(U_input[bottom_line][first_index-1], end="")
+            
         #direct-bottom
         for x in indexes:
             print(U_input[bottom_line][x], end="")
         #bottom-right
-        print(U_input[bottom_line][last_index + 1])
+        if last_index != len(U_input[index1])-1:
+            print(U_input[bottom_line][last_index + 1])
+        else:
+            print()
 
     print()
 
@@ -70,34 +86,38 @@ def find_sp_symbol(U_input, index1, indexes):
     # Checking the top 
     if index1 != 0:
         #top-left
-        if U_input[top_line][first_index-1] != '.':
-            ic("top_left")
-            return True
+        if first_index != 0:
+            if U_input[top_line][first_index-1] != '.':
+                ic("top_left")
+                return True
         #direct-top
         for x in indexes:
             if U_input[top_line][x] != ".":
                 ic(f"direct_top {x}")
                 return True
         #top-right
-        if U_input[top_line][last_index + 1] != '.':
-            ic("top_right")
-            return True
+        if last_index != len(U_input[index1])-1:
+            if U_input[top_line][last_index + 1] != '.':
+                ic("top_right")
+                return True
         
     # Checking the bottom
     if index1 != len(U_input)-1:
         #bottom-left
-        if U_input[bottom_line][first_index-1] != '.':
-            ic("bot_left")
-            return True
+        if first_index != 0:
+            if U_input[bottom_line][first_index-1] != '.':
+                ic("bot_left")
+                return True
         #direct-bottom
         for x in indexes:
             if U_input[bottom_line][x] != ".":
                 ic(f"direct_bot {x}")
                 return True
         #bottom-right
-        if U_input[bottom_line][last_index + 1] != '.':
-            ic("bot_right")
-            return True
+        if last_index != len(U_input[index1])-1:
+            if U_input[bottom_line][last_index + 1] != '.':
+                ic("bot_right")
+                return True
         
     # Checking sides
     #left
@@ -111,15 +131,19 @@ def find_sp_symbol(U_input, index1, indexes):
             ic("right")
             return True
 
-    ic("made it through all the hecks")
+    print("#####    ######  ######  ######  ######  #####")
+    print("##   ##  ##  ##  ###     ###     ####    ##   ##")
+    print("#####    ######    ####    ####  ##      ##   ##")
+    print("##       ##  ##  ######  ######  ######  #####")
     return False
 
 def get_answer_1(U_input):
-    sum = []
+    sum = 0
     number = ""
     indexes = []
     found = False
-    print_given()
+    done = False
+    #print_given()
     for index1, line in enumerate(U_input):
         for index2, symbol in enumerate(line):
             if symbol.isnumeric():
@@ -127,16 +151,30 @@ def get_answer_1(U_input):
 
             if found:
                 if not symbol.isnumeric():
+                    done = True
+                elif (index2 == len(line)-1):
+                    number = number + symbol
+                    indexes.append(index2)
+                    done = True
+
+                if done:
                     found = False
-                    do_box(U_input, index1, indexes)
+                    done = False
                     ic(number)
+                    ic(index1)
                     if find_sp_symbol(U_input, index1, indexes):
-                        sum.append(int(number))
+                        sum+=int(number)
+                        ic(sum)
+                        ic()
+
+                    do_box(U_input, index1, indexes)
+
                     number = ""
                     indexes = []
+                    #time.sleep(0.5)
                     test = input()
                     os.system('cls')
-                    print_given()
+                    #print_given()
                     continue
                 number = number + symbol
                 indexes.append(index2)
@@ -146,7 +184,7 @@ def get_answer_1(U_input):
 
 def main():
     sam = get_answer_1(temp)
-    print(sum(sam))
+    print(sam)
 
 if __name__ == '__main__':
 	main()
